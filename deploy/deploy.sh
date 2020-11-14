47c6443056dc82c887b53f4d71c0ec2afd9ef47b
Mark Parker <godefroi@users.noreply.github.com>
2020-11-14 10:27:32 -0700

    Add debug output to deployment creation

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,5 @@
 az group deployment create \
         --name "$(date +'%Y%m%d-%H%M%S')" \
         --resource-group $1 \
-        --template-file deploy.json
+        --template-file deploy.json \
+        --debug
